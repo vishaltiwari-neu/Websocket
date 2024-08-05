6d79179037ba647de653d14923c83dcce3a8fe4e
vishal <tiwari.vish@northeastern.edu>
2024-08-05 19:54:55 -0400

    First working websocket

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, leave_room, emit
 from flask_cors import CORS, cross_origin
 from datetime import datetime
+from flask import request
 
 from services.room_service import Rooms
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -19,7 +22,51 @@ def index():
 
 @app.route('/bidding.html')
 def bidding():
-    return render_template('bidding.html')
+    user = request.args.get('userName')
+    room = request.args.get('roomName')
+    if user and room:
+        return render_template('bidding.html', user=user, room=room)
+    else:
+        return "Missing userName or roomName", 400
+
+
+@app.route('/completeAuction', methods=['POST'])
+def complete_auction():
+    room = request.json.get('roomName')
+    bidder = request.json.get('bidder')
+    bid = request.json.get('bid')
+
+    if not(room and bidder and bid):
+        return 400
+    
+    rooms.complete_auction(room, {'user': bidder, 'bid': bid, 'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')})
+
+
+@app.route('/createRoom', methods=['POST'])
+def create_room():
+    room = request.json.get('roomName')
+    emails = request.json.get('users')
+    admin = request.json.get('admin')
+    print (room, emails, admin)
+    if room:
+        rooms.create_room(room, emails, admin)
+        
+        return "Done", 200
+    else:
+        return "Missing roomName", 400
+
+@app.route('/checkUser', methods=['GET'])
+def check_user():
+    user = request.args.get('userName')
+    room = request.args.get('roomName')
+    print (user, room)
+    if user and room:
+        data = rooms.check_user_in_room(user, room)
+        print(data)
+        return data, 200
+    
+    else:
+        return {"error": "Missing userName or roomName"}, 400
 
 @socketio.on('joinRoom')
 def handle_join_room(data):
@@ -28,44 +75,64 @@ def handle_join_room(data):
     room = data['room']
     
     # Add user to the room
-    rooms.add_user_to_room(user, room)
+    rooms.add_user_to_room(user, room, request.sid)
     
     join_room(room)
     total_users = rooms.get_users_in_room(room)
+    print("AAAAAAA", total_users)
     emit('user_list', total_users, to=room)
-    emit('chatMessage', f'{user} has entered the room.', to=room)
+    emit('notification', f'{user} has entered the room.', to=room)
     print(f'{user} joined room: {room}')
 
-@socketio.on('leaveRoom')
-def handle_leave_room(data):
-    user = data['user']
-    room = data['room']
-    
-    # Remove user from the room
-    rooms.remove_user_from_room(user, room)
-    
-    leave_room(room)
-    total_users = rooms.get_users_in_room(room)
-    emit('user_list', total_users, to=room)
-    emit('chat_message', f'{user} has left the room.', to=room)
-    print(f'{user} left room: {room}')
 
 @socketio.on('chatMessage')
 def handle_chat_message(data):
     user = data['user']
     room = data['room']
     msg = data['message']
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     print(f'{user} sent message: {msg} in room: {room}')
     emit('chatMessage', {'user': user, 'message': msg, 'timestamp': timestamp}, room=room)
 
 @socketio.on('placeBid')
-def handle_chat_message(data):
+def handle_bid(data):
     user = data['user']
     room = data['room']
     bid = data['bid']
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    emit('placeBid', {'user': user, 'msg': bid, 'timestamp': timestamp}, to=room)
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    current_bid = bid
+    last_activity = rooms.get_activity(room)
+    if last_activity and last_activity['timestamp'] >= timestamp:
+        print ("Bid is older than current bid")
+        emit('currentBid', {'user': user, 'bid': last_activity['bid'], 'timestamp': timestamp}, to=room)
+        return
+    
+    elif last_activity is {}:
+        print ("No activity")
+        current_bid = bid
+    
+    elif last_activity and last_activity['timestamp'] < timestamp:
+        current_bid = bid + last_activity['bid']
+    
+    rooms.add_activity(room, {'user': user, 'bid': current_bid, 'timestamp': timestamp})
+    
+    # emit('placeBid', {'user': user, 'bid': bid, 'timestamp': timestamp}, to=room)
+    emit('currentBid', {'user': user, 'bid': current_bid, 'timestamp': timestamp}, to=room)
+
+
+@socketio.on('leave')
+def handle_disconnect(data):
+    # Identify the user and room from the socket session
+    print("Disconnect", request.sid)
+    user = rooms.get_room_from_request_id(request.sid)
+    if user:
+        leave_room(user['room'])
+        rooms.remove_user_from_room(user['name'], user['room'], request.sid)
+        total_users = rooms.get_users_in_room(user['room'])
+        emit('user_list', total_users, to=user['room'])
+        emit('notification', {'user': 'System', 'message': f'{user["name"]} has left the room.', 'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, room=user['room'])
+        print(f'{user["name"]} disconnected from room: {user["room"]}')
+
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0')
--- services/room_service.py
+++ services/room_service.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+import redis
+import json
+from pymongo import MongoClient
+
 class Rooms:
 
     def __init__(self):
-        self.rooms = {}
+        self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
+        self.redis_client_room = redis.Redis(host='localhost', port=6379, db=1)
+        self.redis_client_activity = redis.Redis(host='localhost', port=6379, db=2)
+        self.mongo_client = MongoClient('localhost', 27017)
     
     def add_room(self, room):
         if room not in self.rooms:
@@ -11,19 +19,66 @@ class Rooms:
         if room in self.rooms:
             del self.rooms[room]
     
-    def add_user_to_room(self, user, room):
-        if room in self.rooms:
-            self.rooms[room].add(user)
-        else:
-            self.rooms[room] = set([user])
+    def add_user_to_room(self, user, room, request_id):
+        user_body = json.dumps({'name': user, 'room': room})
+        self.redis_client.set(request_id, user_body)
+        self.redis_client_room.sadd(room, user)
     
-    def remove_user_from_room(self, user, room):
-        if room in self.rooms and user in self.rooms[room]:
-            self.rooms[room].remove(user)
+    def remove_user_from_room(self, user, room, request_id):
+        self.redis_client.delete(request_id)
+        self.redis_client_room.srem(room, user)
     
     def get_users_in_room(self, room):
-        if room in self.rooms:
-            return list(self.rooms[room])
-        return []
+        users = self.redis_client_room.smembers(room)
+        users = [user.decode('utf-8') for user in users]
+        return users
 
+    def get_rooms(self):
+        rooms = self.redis_client_room.keys()
+        rooms = [room.decode('utf-8') for room in rooms]
+        return rooms
     
+    def add_activity(self, room, activity):
+        key_name = room + '_activity'
+        activity_json = json.dumps(activity)
+        self.redis_client_activity.set(key_name, activity_json)    
+
+    def get_activity(self, room):
+        key_name = room + '_activity'
+        activity = self.redis_client_activity.get(key_name)
+        print("Activity")
+        return json.loads(activity) if activity else {}
+    
+    def get_room_from_request_id(self, request_id):
+        res = self.redis_client.get(request_id)
+        if res:
+            return json.loads(res)
+        return {}
+
+    def check_user_in_room(self, user, room):
+        doc = self.mongo_client['auction']['rooms'].find_one({'users': user, 'name': room})
+        if doc:
+            del doc['_id']
+            doc["isEmpty"] = False
+        else:
+            doc = {}
+            doc["isEmpty"] = True
+        return doc if doc else {}
+     
+    def create_room(self, room, users, admin):
+        room_obj = {}
+        room_obj['name'] = room
+        room_obj['users'] = users
+        room_obj['createdBy'] = admin
+        room_obj['createdAt'] = datetime.now()
+        room_obj['completed'] = False
+        room_obj['winner'] = {}
+        room_obj['users'].append(admin) # Add admin to the list of users
+        self.mongo_client['auction']['rooms'].insert_one(room_obj)
+    
+    def complete_auction(self, room, data):
+        winner = {}
+        winner['user'] = data['user']
+        winner['bid'] = data['bid']
+        winner['timestamp'] = datetime.now()
+        self.mongo_client['auction']['rooms'].update({'name': room}, {'$set': {'completed': True, 'winner': winner}})
